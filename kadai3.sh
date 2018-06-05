e31e5cf06c0bdc11eab0ed1d74f0d6c8a5198c6d
Liujiajian <a1748jl@aiit.ac.jp>
2018-06-05 22:25:08 +0900

    upload kadai3

---
+++ kadai3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Please input number:" i
+j=2
+flag=1
+while [ $j -le `expr $i / 2` ]
+do
+if [ `expr $i % $j` -eq 0 ]
+    then flag=0;
+    echo "${i} is not a Prime"
+    break;
+fi
+j=`expr $j + 1`
+done
+if [ $flag -eq 1 ]
+then
+    echo "${i} is a Prime"
+fi
+i=`expr $i + 1`
