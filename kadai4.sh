33db94187c7949879f54032caa40bc6e288c8b28
Liujiajian <a1748jl@aiit.ac.jp>
2018-06-16 21:30:25 +0900

    The file changed.

--- kadai4.sh
+++ kadai4.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-if [! -n "$1"];then
+
+if [ $1 = ! ];then
 	echo "You have not input the number!"
-else
-	echo " $(sh ./kadai3.sh $1)"
-	backtime=`date +%Y%m%d%H%M%S`
-	git add .
-	git commit -m bk_${backtime}
-	git push origin master
 fi
+test=$(sh ./kadai3.sh $1)
+echo $test
+if [ $1 = 4 -a  "$test" = "4 is not a Prime" ];then
+		exit 0
+else
+		exit 1
+fi
+backtime=`date +%Y%m%d%H%M%S`
+git add .
+git commit -m bk_${backtime}
+git push origin master
+
 
